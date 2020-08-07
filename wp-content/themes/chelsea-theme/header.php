aa36c7a783b108e322ce484de1d548e9229e816f
Chelsea <cej48@drexel.edu>
2020-08-07 09:36:02 -0400

    Initial commit

---
+++ wp-content/themes/chelsea-theme/header.php
@@ -0,0 +1,28 @@
+  
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Chelsea James</title>
+ 
+
+  <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<header class="header">
+  <div class="container header__content">
+    <img class="header__logo" src="https://placehold.it/500x6" alt="This is the logo">
+    <nav class= "header__nav main__nav">
+     <ul class= " main-nav__group">
+        <li class= "main-nav__item"> <a href="#" class= "main-nav__link">About</a></li>
+        <li class= "main-nav__item"> <a href="#" class= "main-nav__link">Portfolio</a></li>
+       <li class= "main-nav__item"> <a href="#" class= "main-nav__link">Resume</a></li>
+       <li class= "main-nav__item"> <a href="#" class= "main-nav__link">Contact</a></li>
+
+
+       </ul>
+
+    <?php wp_nav_menu(['theme_location' => 'primary_menu']);?>
+  </div>
+</header>
